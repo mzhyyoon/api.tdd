fe941b49db1349a696b70533123d7f4e68e44bb3
hyyoon <hyyoon@mz.co.kr>
2018-09-17 17:56:10 +0900

    [hyyoon] login -> ing

--- routes/users.js
+++ routes/users.js
@@ -30,4 +30,45 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.post('/signin', (req, res) => {
+    const user = db.get().collection('users');
+
+    user.find({
+        email : decodeURIComponent(req.body.email)
+    }).toArray((err, user) => {
+        if(err) {
+            throw err;
+        }
+
+        if(user.length === 0) {
+            res.status(406)
+                .json({
+                    errors : {
+                        status: 406,
+                        message: 'Please Check. Id or Password.'
+                    }
+                })
+                .end();
+        } else {
+            if(user[0].password !== decodeURIComponent(req.body.password)) {
+                res.status(500)
+                    .json({
+                        errors : {
+                            status: 500,
+                            message: 'Please Check. Id or Password.'
+                        }
+                    })
+                    .end();
+            } else {
+                res.status(200)
+                    .json({
+                        status : 200,
+                        user
+                    })
+                    .end();
+            }
+        }
+    })
+});
+
 module.exports = router;
