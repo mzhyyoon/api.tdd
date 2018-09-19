3de44b566974cb0fb403033048a4526a635c3c0d
hyyoon <hyyoon@mz.co.kr>
2018-09-19 17:57:00 +0900

    [hyyoon] fixed

--- routes/testcases.js
+++ routes/testcases.js
@@ -101,7 +101,7 @@ router.post('/', (req, res) => {
     const testcases = db.get().collection('testcases');
 
     cmd.get(
-        'mocha --recursive ./test/specs/*.js --reporter json --timeout 20000',
+        'mocha --recursive ./test/specs/*.js --reporter json',
         (err, data) => {
             const result = JSON.parse(data);
 
--- test/specs/main.spec.js
+++ test/specs/main.spec.js
@@ -3,30 +3,41 @@ require('chromedriver');
 const {Builder} = require('selenium-webdriver');
 const {assert} = require('chai');
 
-let driver = new Builder()
-    .withCapabilities({
-        browserName: 'chrome',
-        chromeOptions: {
-            args : ['headless','disable-gpu'],
-            mobileEmulation: {
-                deviceName: 'iPhone 6/7/8 Plus'
-            }
-        }
-    })
-    .build();
+describe('Mobile > Main', function () {
+    this.timeout(30000);
+    let driver;
 
-describe('Mobile > Main', () => {
-    it('Page Title 은 "롯데마트몰 - easy & slow life" 인가? -> success', async () => {
-        await driver.get('http://m.lottemart.com/mobile/corners.do');
-        await driver.getTitle().then(async (title) => {
-            await assert.equal(title, '롯데마트몰 - easy & slow life');
+    before(function () {
+        driver = new Builder()
+            .withCapabilities({
+                browserName: 'chrome',
+                chromeOptions: {
+                    args: ['headless', 'disable-gpu'],
+                    mobileEmulation: {
+                        deviceName: 'iPhone 6/7/8 Plus'
+                    }
+                }
+            })
+            .build();
+    });
+
+    after(function () {
+        return driver.quit();
+    });
+
+    it("Open : 롯데마트몰 > 모바일", function () {
+        return driver.get('http://m.lottemart.com/mobile/corners.do');
+    });
+
+    it('Page Title 은 "롯데마트몰 - easy & slow life" 인가? -> success', function () {
+        return driver.getTitle().then(function(title) {
+            assert.equal(title, '롯데마트몰 - easy & slow life');
         });
     });
 
-    it('Page Title 은 "롯데마트몰 - easy & slow life" 인가? -> fail', async () => {
-        await driver.get('http://m.lottemart.com/mobile/corners.do');
-        await driver.getTitle().then(async (title) => {
-            await assert.equal(title, 'lottemart');
+    it('Page Title 은 "롯데마트몰 - easy & slow life" 인가? -> fail', function () {
+        return driver.getTitle().then(function(title) {
+            assert.equal(title, 'lottemart');
         });
     });
 });
