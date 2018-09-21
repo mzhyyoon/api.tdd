6d9c8f61b2d5b185dd9ab449ada69f1c3bbbca25
hyyoon <hyyoon@mz.co.kr>
2018-09-21 13:01:41 +0900

    [hyyoon] edit -> spec : chrome

--- test/specs/detail.spec.js
+++ test/specs/detail.spec.js
@@ -9,16 +9,25 @@ describe('Mobile4 > Main', function () {
     before(async function () {
         browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
         page = await browser.newPage();
-        await page.goto('http://m.lottemart.com/mobile/corners.do');
     });
 
     after(function () {
         return browser.close();
     });
 
-    it('Page Title 은 "롯데마트몰 - easy & slow life" 인가? -> success', function () {
+    it('Page Title 은 "롯데마트몰 - easy & slow life" 인가? -> success', async function () {
+        await page.goto('http://m.lottemart.com/mobile/corners.do');
+
         return page.title().then(function(title) {
             assert.equal(title, '롯데마트몰 - easy & slow life');
+        });
+    });
+
+    it('naver test??!', async function () {
+        await page.goto('http://naver.com');
+
+        return page.title().then(function(title) {
+            assert.equal(title, 'Naver');
         });
     });
 });
